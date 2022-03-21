6c8c7330ac0f2cb39d1be0ead5c7f2a0fc09e9a3
Aleksandr Moskalchuk <moskalchuk.aleksandr@gmail.com>
2022-03-22 01:54:09 +0300

    connection config server

--- build/generated/sources/annotationProcessor/java/main/com/back/board/mapping/PersonMappingImpl.java
+++ build/generated/sources/annotationProcessor/java/main/com/back/board/mapping/PersonMappingImpl.java
@@ -7,7 +7,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2022-03-08T23:34:07+0300",
+    date = "2022-03-22T01:11:47+0300",
     comments = "version: 1.4.2.Final, compiler: IncrementalProcessingEnvironment from gradle-language-java-7.3.3.jar, environment: Java 17.0.1 (Oracle Corporation)"
 )
 @Component
--- src/main/java/com/back/board/controller/PersonController.java
+++ src/main/java/com/back/board/controller/PersonController.java
@@ -4,6 +4,7 @@ import com.back.board.dto.PersonDto;
 import com.back.board.service.PersonService;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,22 +14,30 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @Slf4j
-@AllArgsConstructor
+//@AllArgsConstructor
 @RestController
 @RequestMapping("/person")
 public class PersonController {
 
-    private final PersonService personService;
+//    private final PersonService personService;
 
-    @GetMapping
-    public String getMessage(){
-        return "Hello";
-    }
+    @Value("${properties.test}")
+    private String test;
+//
+//    @GetMapping
+//    public String getMessage(){
+//        return "Hello";
+//    }
+//
+//    @PostMapping
+//    public ResponseEntity<String> save(@RequestBody PersonDto personDto) {
+//        var status = personService.saveNewPerson(personDto);
+//        return new ResponseEntity<>(status, HttpStatus.OK);
+//
+//    }
 
-    @PostMapping
-    public ResponseEntity<String> save(@RequestBody PersonDto personDto) {
-        var status = personService.saveNewPerson(personDto);
-        return new ResponseEntity<>(status, HttpStatus.OK);
-
+    @RequestMapping("/test")
+    public void getTest(){
+        log.info(test);
     }
 }
